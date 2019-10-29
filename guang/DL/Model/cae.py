9f8747ef4a63f0c81e5c66567ac35ce0ebfba1fd
beidongjiedeguang <kunyuany@163.com>
2019-10-30 00:51:04 +0800

    add some func

--- guang/DL/Model/cae.py
+++ guang/DL/Model/cae.py
@@ -28,47 +28,30 @@ class CAE:
 		self.autoencoder = Model(input_img, decoded)
 		self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy') 
 	
-	def fit(self,x_train, x_test):
+	def fit(self,x_train, x_test, epochs=5, batch_size=128):
 
 		self.autoencoder.fit(x_train, x_train,
-						epochs=5,
-						batch_size=128,
+						epochs=epochs,
+						batch_size=batch_size,
 						shuffle=True,
 						validation_data=(x_test, x_test)
 					)
 
 
-def TEST(SIZE=128):
+def TEST(SIZE=64):
 	'''
 	SIZE: Specify image size
 	'''
-	from tensorflow.keras.datasets import mnist
-	import numpy as np
+	import sys
+	sys.path.append('../..')
+	from Utils.load_data import load_mnist
 	import matplotlib.pyplot as plt
-	(x_train, _), (x_test, _) = mnist.load_data()
-
-	x_train = x_train.astype('float32') / 255.
-	x_test = x_test.astype('float32') / 255.
-	x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
-	x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
-
-	x_train = x_train[:10000]
-	x_test = x_test[:1000]
-	import cv2
-	X_train, X_test = x_train[0][...,-1], x_test[0][...,-1]
-	X_train, X_test = cv2.resize(X_train, (SIZE,SIZE)), cv2.resize(X_test, (SIZE,SIZE))
-	X_train, X_test = X_train.reshape(1,SIZE,SIZE,1), X_test.reshape(1,SIZE,SIZE,1)
-	for i, j in zip(x_train, x_test):
-		x_i = cv2.resize(i[...,-1], (SIZE, SIZE))
-		x_j = cv2.resize(j[...,-1], (SIZE, SIZE))
-		X_train = np.concatenate((X_train, x_i.reshape(1,SIZE,SIZE,1)), axis=0)
-		X_test = np.concatenate((X_test, x_j.reshape(1,SIZE,SIZE,1)), axis=0)
-		
-	x_train, x_test = X_train, X_test
-	w, h, input_channel = 28, 28, 1
+	x_train, x_test = load_mnist(SIZE=SIZE)
+	tb,th,tw,tc = x_test.shape
+	w, h, input_channel = th,tw,1
 	cae = CAE(w, h, input_channel)
 	cae.compile()
-	cae.fit(x_train, x_test)
+	cae.fit(x_train, x_test,epochs=1)
 
 	decoded_imgs = cae.autoencoder.predict(x_test[:20])
 
