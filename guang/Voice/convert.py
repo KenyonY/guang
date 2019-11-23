dd953fbdd6a7be164df766753db0fe046d965a51
beidongjiedeguang <kunyuany@163.com>
2019-11-23 16:39:10 +0800

    casual

--- guang/Voice/convert.py
+++ guang/Voice/convert.py
@@ -1,8 +1,8 @@
 import os
 from pydub import AudioSegment
 
+
 def cvt2wav(orig_path, new_path, sr=16000):
-    
     basename = os.path.basename(orig_path)
     sufname = basename.split('.')[-1]
 
@@ -11,3 +11,7 @@ def cvt2wav(orig_path, new_path, sr=16000):
     x = x.set_frame_rate(sr)
     x = x.set_sample_width(2)
     x.export(new_path, format='wav')
+
+
+if __name__ == "__main__":
+    pass
