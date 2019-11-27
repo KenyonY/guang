37ae3e2fb4af59924facbfa84c46eeccb6139522
beidongjiedeguang <kunyuany@163.com>
2019-11-28 00:27:14 +0800

    ^ ^

--- guang/wechat/test.py
+++ guang/wechat/test.py
@@ -1,11 +1,17 @@
 import guang
 from guang.wechat.Utils import * 
-itchat.auto_login(hotReload=True)
-while d_time(60):
-    nickName = '光'
-    msg = dynamic_specified_msg(get_userName(nickName)[nickName])
-    msg = download_file(msg)
+import argparse
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n','--name', help='nike name', type=str, default='光')
+    args = parser.parse_args()
+    nickName = args.name
 
+    itchat.auto_login(hotReload=True)
 
-
+    while d_time(60):
+        
+        msg = dynamic_specified_msg(get_userName(nickName)[nickName])
+        msg = download_file(msg)
+        
