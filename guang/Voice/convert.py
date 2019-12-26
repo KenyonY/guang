de1f1f7655fcdd83a556ae13e3c3b825f9fb02ec
beidongjiedeguang <kunyuany@163.com>
2019-12-27 00:20:29 +0800

    add multiprocessing for cvt2wav

--- guang/Voice/convert.py
+++ guang/Voice/convert.py
@@ -1,6 +1,6 @@
 import os
 from pydub import AudioSegment
-
+from multiprocessing import Pool
 
 def cvt2wav(orig_path, new_path, sr=16000):
     basename = os.path.basename(orig_path)
@@ -12,7 +12,12 @@ def cvt2wav(orig_path, new_path, sr=16000):
     x = x.set_sample_width(2)
     x.export(new_path, format='wav')
 
-
+def multi_cvt2wav(PATH_orig, PATH_new, sr=16000, n_cpu=16):
+    p =Pool(processes=n_cpu)
+    for i,j in zip(PATH_orig, PATH_new):
+        p.apply_async(cvt2wav, (i, j, sr))
+    p.close()
+    p.join()
 
 if __name__ == "__main__":
     pass
