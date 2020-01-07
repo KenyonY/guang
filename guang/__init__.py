d122fb887b7a7ab385754fe389d298e641a17f47
beidongjiedeguang <kunyuany@163.com>
2020-01-07 18:21:04 +0800

    'update'

--- guang/__init__.py
+++ guang/__init__.py
@@ -9,7 +9,7 @@ __v = get_version.get_version(update=False)
 
 # '0.0.7.2.7'
 __version__ = '0.0.' + __v
-print('__version__:', __version__)
+print('guang.__version__=:', __version__)
 __author__ = 'K.y'
 __copyright = 'Copyright 2019 K.y'
 
