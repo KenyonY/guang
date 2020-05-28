aa51af952d85ba6920b1003a860cca0d56963da2
beidongjiedeguang <kunyuany@163.com>
2020-05-29 00:07:06 +0800

    add some useful functions

--- guang/cv/utils.py
+++ guang/cv/utils.py
@@ -2,6 +2,46 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+def rotate(image, angle, center=None, scale=1.0):
+    """Rotate the image angle degrees.
+    if the center is None, initialize it as the center of
+    the image
+    """
+    (h, w) = image.shape[:2]
+
+    if center is None:
+        center = (w // 2, h // 2)
+
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h))
+    return rotated
+
+def rotate_bound(image, angle):
+    """Keep the boundary and rotate angle degrees clockwise"""
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w / 2, h / 2)
+
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+
+    return cv2.warpAffine(image, M, (nW, nH))
+
+def auto_canny(image, sigma=0.33):
+    
+    v = np.median(image)
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    return edged
+
 def cvtBlackWhite(fileName):
     '''
     convert image's black color to white and meanwhile convert white to black.
