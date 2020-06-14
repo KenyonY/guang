af3653c555904aaa73d32e1933ac49cbb5ec36a2
beidongjiedeguang <kunyuany@163.com>
2020-06-14 20:59:05 +0800

    The concise

--- night_workflow.py
+++ night_workflow.py
@@ -9,28 +9,10 @@ rm('build')
 rm('dist')
 rm('guang.egg-info')
 
-os.system('python setup.py sdist bdist_wheel')
-upgrade = f'pip uninstall guang -y && pip install dist/guang-{guang.__version__}-py3-none-any.whl'
-os.system(upgrade)
+os.system('pip uninstall guang -y && python setup.py install')
+
 rm('build')
 rm('guang.egg-info')
 
 
-"""
->>> res = os.popen(commend) # use os 更简洁
->>> res = subprocess.Popen(commend, shell=True, stdout=subprocess.PIPE).stdout   # use subprocess 适用于更复杂情况
->>> print(res.read())
-# or 
->>> res = subprocess.getoutput(commend)
-# ps
-returncode = subprocess.call(commend)
-# is equal to 
-returncode = os.system(commend)
-
-# 执行结果保存文件
->>> fhandle = open(filename, "w")
->>> pipe = subprocess.Popen(commend, shell=True, stdout=fhandle).stdout
->>> fhandle.close()
-
-"""
 
