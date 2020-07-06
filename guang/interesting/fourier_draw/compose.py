d9567b56ed14b72218f26839732245e398ebec4e
beidongjiedeguang <kunyuany@163.com>
2020-07-06 17:19:08 +0800

    daily update

---
+++ guang/interesting/fourier_draw/compose.py
@@ -0,0 +1,74 @@
+from manimlib.imports import *
+from PIL import Image
+import numpy as np
+import imageio
+from guang import path
+from skimage.transform import hough_circle, hough_circle_peaks
+from skimage.feature import canny
+import cv2
+from guang import implot, auto_canny
+from skimage import measure
+from skimage import filters
+import matplotlib.pyplot as plt
+from guang import rm
+
+
+def get_data_from_func():
+    '''
+    love
+    '''
+    n = 150
+    t = np.linspace(0, 2 * np.pi, n)
+    x = 16 * (np.sin(t)) ** 3
+    y = 13 * np.cos(t) - 5 * np.cos(2 * t) - 2 * np.cos(3 * t) - np.cos(4 * t)
+    X = x + 1j * y
+    return X
+
+def from_text(S = "LOVE", scale=2, style=None):
+    """get text contour list
+    """
+    text = TextMobject(S).scale(scale)
+    img = text.get_image()
+    gray_img = np.array(img.convert("L"))
+    contours = measure.find_contours(gray_img, 120)
+
+    except_list = ["*.aux", "*.log", "*.svg", "*.tex", "*.xdv"]
+    print(f"Deleting {except_list} in directory \n{os.getcwd()}")
+    [rm(i) for i in except_list]
+    X = []
+    for contour in contours:
+        x, y = contour[:, 1], -contour[:, 0]
+        X.append(x+1j*y)
+    return X
+
+
+def from_pic():
+    pass
+
+
+def from_csv():
+    pass
+
+def sort_data(X):
+    '''
+    对于图数据，使用最短路径算法得到序列。
+    然后返回二元数(2维)或三元数(三维)形式
+    其定义在guang.geo.space中
+    '''
+    return X
+
+
+def clean_data(X):
+    X = sort_data(X)
+    x, y = X.real, X.imag
+    x = (x - x.min()) / (x.max() - x.min()) - 0.5
+    y = (y - y.min()) / (y.max() - y.min()) - 0.5
+    X = x + 1j * y
+    return X
+
+if __name__ == "__main__":
+    X = from_text("533", 5)
+    plt.figure(figsize=(10, 5))
+    for x in X:
+        plt.plot(x.real, x.imag, linewidth=2)
+    plt.axis("equal")
--- guang/interesting/fourier_draw/fourier_line.py
+++ guang/interesting/fourier_draw/fourier_line.py
@@ -1,31 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from guang.interesting.fourier_draw.clean_data import get_data_from_func, clean_data
+from compose import get_data_from_func, clean_data, from_text
 from matplotlib.animation import FuncAnimation
+
+from matplotlib import rc
+# rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+# rc('text', usetex=True)
+# plt.style.use(["science"])
+
+major_tick_dict = dict(which='major', direction='in', top=1, right=1, length=5, width=0.3, labelsize=10)
+minor_tick_dict = dict(which='minor', direction='in', top=1, right=1, length=3, width=0.2, labelsize=10)
 
 def draw(X, fig, ax, large_to_small = True):
     n = len(X)
     F = np.fft.fft(X)
 
-    F[np.abs(F) < 1] = 0
+    F[np.abs(F) < 10] = 0
     N = (np.abs(F) > 0).sum()
     print('总点数{}，过滤后的有效点数{}'.format(n, N))
 
-    # pic_total = ax.plot(X.real, X.imag, linestyle=':', color='gray')  # 完整图像
-    # pic_total = ax.plot([0],[0])
+    pic_total = ax.plot(X.real, X.imag, linestyle=':', color='gray')  # 完整图像
+    pic_total = ax.plot([0],[0])
     
     # 已经画出来的图像，后面填充
-    pic_drawn = ax.plot([0], [0], '-r', linewidth=1.5)
+    pic_drawn = ax.plot([0], [0], '-r', linewidth=1.2)
     # n-1个圆周,后面填充
     circle_plt_list = [ax.plot([0], [0], '-', color="lightgrey",linewidth=0.5)[0] for i in range(n - 1)]
     # 直径线，后面填充
     radius_plt = ax.plot([0,0], [0,0], color='r', marker='.', linestyle='-', linewidth=0.7, markersize=3)
     #
-    # plt.ion()
+    # ax.set_xlim(0, 1000)
+    # ax.set_ylim(-0.5, 0.5)
+    plt.ion()
 
-    def update_all(xi):
-        # fs =
-        # w_max =
+    def update_part(xi):
+
         w = 2 * np.pi * np.arange(n) / n
         dw = 1/n
         Fedw = F * np.exp(1j *xi * w) * dw
@@ -55,27 +64,38 @@ def draw(X, fig, ax, large_to_small = True):
             plt.setp(circle_plt_list[j], 'xdata', circle_point.real, 'ydata', circle_point.imag)
         return pic_drawn + radius_plt + circle_plt_list
 
-        # return radius_plt
+    return update_part
 
-    return update_all
+def hyeves(data, large_to_small=1, fps=12):
+    fig, ax = plt.subplots(1, 1, figsize=(7,5))
+    plt.minorticks_on()
+    ax.tick_params(**major_tick_dict)
+    ax.tick_params(**minor_tick_dict)
+    # for X in data:
+    X = data[0]
+    # print(X, X.shape)
+    # plt.plot(X.real, X.imag)
+    update_part = draw(X, fig, ax, large_to_small=large_to_small)
+    ani = FuncAnimation(fig, update_part, blit=True, interval=10, frames=len(X))
 
-
-if __name__ == '__main__':
-    from matplotlib import animation
-    # 导入数据
-    X = get_data_from_func()
-    # 清洗数据
-    X = clean_data(X)
-    fig, ax = plt.subplots(1, 1)
-    update_all = draw(X, fig, ax, large_to_small=0)
-    print(update_all)
-    ani = FuncAnimation(fig, update_all, blit=True, interval=10, frames=len(X))
     plt.axis("equal")
-    plt.xlim(-0.8, 0.8)
-    plt.ylim(-0.8, 0.85)
-    # ani.save('fourier.gif', writer='pillow', dpi=200, fps=10)
-    ani.save('fourier0.gif', writer='imagemagick',dpi=200, fps=15)
+    # plt.xlim(-1000, 1000)
+    # plt.xlim(-0.8, 0.8)
+    # plt.ylim(-0.8, 0.85)
+
+    # plt.axis('off')
+    # plt.xticks([])
+    # plt.yticks([])
+
+    # ani.save(f'fourier{large_to_small}.gif', writer='pillow', dpi=10, fps=fps)
+
+    # ani.save(f'fourier{large_to_small}.gif', writer='imagemagick',dpi=200, fps=fps)
 
     # ani.save('fourier.avi', writer = animation.FFMpegWriter(),dpi=200, fps=10)
-
     plt.show()
+if __name__ == '__main__':
+    # X = get_data_from_func()
+    # hyeves(X, 1)
+    # hyeves(X, 0)
+    data = from_text("533", 2)
+    hyeves(data)
